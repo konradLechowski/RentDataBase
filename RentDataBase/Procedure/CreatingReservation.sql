3516a59f41db4ac6cf5d62d61e19e739074d11f1
Konrad Lechowski <konradlechowski98@gmail.com>
2021-06-21 07:54:47 +0200

    creating reservation

--- RentDataBase/Procedure/CreatingReservation.sql
+++ RentDataBase/Procedure/CreatingReservation.sql
@@ -2,12 +2,22 @@ CREATE PROCEDURE [dbo].CreatingReservation
 	@Date_from date,
 	@Date_to date,
 	@Worker_id int,
-	@Iteam_id int
+	@Iteam_name varchar(20)
 	/* format daty yyyymmdd*/
 	
 AS
+	select top 1 id from Iteam
+	where name = @Iteam_name 
+	and Availability_id = 1 
+	and Condition_id = 1;
+
 	Insert into Reservation
 	(Date_from, Date_to, Worker_id, Iteam_id)
-	values (@Date_from, @Date_to,@Worker_id, @Iteam_id);
+	values (@Date_from, @Date_to,@Worker_id, Iteam.id);
+	
+	update Iteam
+	set Availability_id = 3
+	where id = Iteam.id;
+
 
 GO;
--- RentDataBase/Procedure/UpdateIteam.sql
+++ RentDataBase/Procedure/UpdateIteam.sql
@@ -1,13 +1,15 @@
 CREATE PROCEDURE [dbo].UpdateIteam 
 	@name varchar(20),
 	@Availbility_id int,
-	@Condition_id int
+	@Condition_id int,
+	@id int
 	
 AS
 	Update  Iteam
 	set  name = @name,
 	Availbility_id = @Availbility_id,
-	Condition_id = @Condition_id;
+	Condition_id = @Condition_id
+	where id = @id;
 
 GO;
 
